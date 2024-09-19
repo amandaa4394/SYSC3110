2c8a11a959a69b7e4e84bb0a149052a6492f7049
amanda <amandapiazza44@gmail.com>
2024-09-19 13:21:49 -0400

    msg

---
+++ AddressBook.java
@@ -0,0 +1,12 @@
+public class AddressBook {
+    BuddyInfo[] buddies;
+
+    public void addBuddy(){}
+
+    public void removeBuddy(){}
+
+    public static void main(String[] args){
+        System.out.println("Address Book");
+    }
+
+}
