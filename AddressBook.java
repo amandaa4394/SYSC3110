2765c4b00172b8a4ffb83ff7316d7f5590b36c3c
amandaa4394 <amandaa4394@gmail.com>
2024-09-26 12:42:37 -0400

    commit

--- AddressBook.java
+++ AddressBook.java
@@ -1,15 +1,29 @@
+import java.util.ArrayList;
+
 public class AddressBook {
-    BuddyInfo[] buddies;
 
-    public void addBuddy(BuddyInfo buddy){}
+    private ArrayList<BuddyInfo> myBuddies;
+    public AddressBook(){
+        myBuddies = new ArrayList<>();
+    }
+    public void addBuddy(BuddyInfo aBuddy){
+        if(aBuddy != null){
+            myBuddies.add(aBuddy);
+        }
+    }
 
-    public void removeBuddy(BuddyInfo buddy){}
+    public BuddyInfo removeBuddy(int index){
+        if(index >= 0 && index < myBuddies.size()){
+            return myBuddies.remove(index);
+        }
+        return null;
+    }
 
     public static void main(String[] args){
-        BuddyInfo buddy = new BuddyInfo("Tom", "1 Ave", 613);
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", 613);
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
-        addressBook.removeBuddy(buddy);
+        addressBook.removeBuddy(0);
 
     }
 }
